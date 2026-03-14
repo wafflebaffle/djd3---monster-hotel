cd8479642b159ab741a4e44aa28d337327b4589b
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-03-14 00:56:44 +0000

    added very simple settings button code

--- Assets/Tech/Scripts/Menus/Main_Menu.cs
+++ Assets/Tech/Scripts/Menus/Main_Menu.cs
@@ -3,8 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject settingsScreen;
     public void StartGame(int sceneindex)
     {
         SceneManager.LoadScene(sceneindex);
+    }
+
+    public void SettingsScreen()
+    {
+        settingsScreen.SetActive(true);
+    }
+
+    public void ExitSetings()
+    {
+        settingsScreen.SetActive(false);
     }
 }
