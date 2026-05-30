08ad914ee66f0434c489fb1048d3a82f80b87406
Diogo <diogofonseca.gamedesigner@gmail.com>
2026-05-30 23:02:10 +0100

    started to implement new volume sliders

--- Assets/Tech/Scripts/AudioSettings.cs
+++ Assets/Tech/Scripts/AudioSettings.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class AudioSettings : MonoBehaviour
 {
     [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider SFXSlider;
 
     private void Start()
     {
